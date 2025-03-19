daebcdce2c4515849f2049773cf6283e890c3ac9
Sajed BEN YOUSSEF <127627616+BySajed@users.noreply.github.com>
2025-03-19 21:47:49 +0100

    add email + passwd form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import Providers from '@/components/Providers'
+import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeToggle } from '@/components/ui/theme-toggle';
+import { Metadata } from 'next';
 import './globals.css'
 
 // Done after the video and optional: add page metadata
-export const metadata = {
-  title: 'Sentinelle.chat',
-  icons: {
-    icon: '/favicon.ico',
-  }
-}
+export const metadata: Metadata = {
+  title: "Sentinelle.online",
+  description: "Sentinelle.online",
+  icons: {icon: "/favicon.ico"},
+};
 
 export default function RootLayout({
   children,
@@ -15,9 +17,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang="en" suppressHydrationWarning>
+      <head />
       <body>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+        <main>
         <Providers>{children}</Providers>
+        </main>
+        <ThemeToggle />
+      </ThemeProvider>
       </body>
     </html>
   )
